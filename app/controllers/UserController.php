b3687aa5c9cf9cc898086fd8cce4d11a2f0648e6
Leogeox <leovginx@gmail.com>
2025-11-26 15:29:21 +0100

    modified the UserModel.php

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -3,5 +3,59 @@ require_once './app/utils/Render.php';
 
 class UserController
 {
+    use Render;
+    public function findAll(): void
+    {
+        $userModel = new UserModel();
+        $users = $userModel->findAll();
 
+        // Prépatation du tableau à envoyer au layout
+        $data = [
+            'title' => 'Liste des utilisateurs',
+            'users' => $users
+        ];
+
+        // Rendu avec layout
+        $this->renderView('user/all', $data);
+    }
+
+    public function findOneById(int $id): void
+    {
+        $userModel = new UserModel();
+        $user = $userModel->findOneById($id);
+
+        // Prépatation du tableau à envoyer au layout
+        $data = [
+            'title' => 'Un utilisateur',
+            'user' => $user
+        ];
+
+        // Rendu avec layout
+        $this->renderView('user/one', $data);
+    }
+
+    //      private array $user;
+    //     public function logUser(string $email, string $mdp): array
+    // {
+    //     $this->user = [
+    //         'email' => $email,
+    //         'mdp' => $mdp
+    //     ];
+    //     return $this->user;
+    // }
+
+    //     public function createUser(array $data): bool
+    // {
+    //     if ($data != $this->user) {
+    //         $this->user += $data;
+    //     } else if ($data = $this->user) {
+    //         return false;
+    //     }
+    //     return true;
+    // }
+
+    //     public function getAllUsers(): array
+    // {
+    //     return $this->user;
+    // }????
 }
--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -7,47 +7,37 @@ class UserModel extends Bdd
         parent::__construct();
     }
 
-    public function findAll(): array
+    public function createUser(array $data): User|false
+    {
+        $users = $this->co->prepare('SELECT * FROM Users WHERE id = :id LIMIT 1');
+        $users->setFetchMode(PDO::FETCH_CLASS, 'Users');
+
+        if ($data != $users) {
+            $users += $data;
+        } else if ($data = $users) {
+            return false;
+        }
+        return $users->fetch();
+    }
+
+    public function logUser(string $email, string $mdp): array
+    {
+        $users = $this->co->prepare('SELECT * FROM Users WHERE id = :id LIMIT 1');
+        $users->setFetchMode(PDO::FETCH_CLASS, 'Users');
+        $users->execute([
+            'email' => $email,
+            'mdp' => $mdp
+        ]);
+
+        return $users->fetch();
+    }
+
+    public function getAllUsers(): array
     {
         $users = $this->co->prepare('SELECT * FROM Users');
         $users->execute();
 
         return $users->fetchAll(PDO::FETCH_CLASS, 'Users');
     }
-
-    public function findOneById(int $id): User|false
-    {
-        $users = $this->co->prepare('SELECT * FROM Users WHERE id = :id LIMIT 1');
-        $users->setFetchMode(PDO::FETCH_CLASS, 'Users');
-        $users->execute([
-            'id' => $id
-        ]);
-
-        return $users->fetch();
-    }
-
-    //      private array $user;
-    //     public function logUser(string $email, string $mdp): array
-//     {
-//         $this->user = [
-//             'email' => $email,
-//             'mdp' => $mdp
-//         ];
-//         return $this->user;
-//     }
-
-    //     public function createUser(array $data): bool
-//     {
-//         if ($data != $this->user) {
-//             $this->user += $data;
-//         } else if ($data = $this->user) {
-//             return false;
-//         }
-//         return true;
-//     }
-
-    //     public function getAllUsers(): array
-//     {
-//         return $this->user;
-//     }????
 }
+
