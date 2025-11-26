b3be068dbf7aea043fc794b11e435737f6342ef1
Leogeox <leovginx@gmail.com>
2025-11-26 12:38:31 +0100

    updated User and UserModel

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -25,4 +25,29 @@ class UserModel extends Bdd
 
         return $users->fetch();
     }
+
+    //      private array $user;
+    //     public function logUser(string $email, string $mdp): array
+//     {
+//         $this->user = [
+//             'email' => $email,
+//             'mdp' => $mdp
+//         ];
+//         return $this->user;
+//     }
+
+    //     public function createUser(array $data): bool
+//     {
+//         if ($data != $this->user) {
+//             $this->user += $data;
+//         } else if ($data = $this->user) {
+//             return false;
+//         }
+//         return true;
+//     }
+
+    //     public function getAllUsers(): array
+//     {
+//         return $this->user;
+//     }????
 }
--- app/models/user.php
+++ app/models/user.php
@@ -1,40 +1,5 @@
 <?php
 
-// class User
-// {
-//     private array $user;
-//     // private array $allUser;
-
-//     public function __construct()
-//     {
-//         $this->user = [];
-//     }
-
-//     public function logUser(string $email, string $mdp): array
-//     {
-//         $this->user = [
-//             'email' => $email,
-//             'mdp' => $mdp
-//         ];
-//         return $this->user;
-//     }
-
-//     public function createUser(array $data): bool
-//     {
-//         if ($data != $this->user) {
-//             $this->user += $data;
-//         } else if ($data = $this->user) {
-//             return false;
-//         }
-//         return true;
-//     }
-
-//     public function getAllUsers(): array
-//     {
-//         return $this->user;
-//     }
-// }
-
 class User
 {
     private int $id;
