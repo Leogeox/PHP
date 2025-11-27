d218f6d5a2e7b69b77c9dfef919bc7cd8cab9a8e
Leogeox <leovginx@gmail.com>
2025-11-27 18:56:21 +0100

    modified views of reservation

--- app/views/reservation/all.php
+++ app/views/reservation/all.php
@@ -1,10 +1,9 @@
 <?php
 echo "<h1>Liste des reservation</h1>";
 if (count($reservation) > 0) {
-    foreach ($r as $reservation) {
-        echo '<h1>' . $reservation->getId() . '</h1>';
-        echo '<p>' . $reservation->getDateReservation() . '</p>';
-        echo '<p>' . $reservation->getEtat() . '</p>';
+    foreach ($reservation as $r) {
+        echo '<h1>' . $r->getId() . '</h1>';
+        echo '<p>' . $r->getDateReservation() . '</p>';
     }
 } else {
     echo '<p>Aucune reservation</p>';
