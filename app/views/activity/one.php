aa1ba45a4c344acafa1dc0c4a7831c7e897e7d7a
Leogeox <leovginx@gmail.com>
2025-11-28 15:36:37 +0100

    made activity/one.php

--- app/views/activity/one.php
+++ app/views/activity/one.php
@@ -1,12 +1,13 @@
 <?php
-foreach ($activities as $activity) {
-    echo '<h1>' . $activity->getNom() . '</h1>';
-    echo '<p>' . $activity->getPlacesDisponibles() . '</p>';
-    echo '<p>' . $activity->getDescription() . '</p>';
-    echo '<p>' . $activity->getDateTimeDebut() . '</p>';
-    echo '<p>' . $activity->getDuree() . '</p>';
-    // affiche les détails d’une activité et propose le formulaire de réservation.
+if (count($activities) > 0) {
+    foreach ($activities as $activity) {
+        echo '<h3>' . $activity->getNom() . '</h3>';
+        echo '<p>' . $activity->getDescription() . '</p>';
+        echo '<p> Places disponible : ' . $activity->getPlacesDisponibles() . '</p>';
+        echo '<p> Heure début : ' . $activity->getDateTimeDebut() . ' h</p>';
+        echo '<p> Durée : ' . $activity->getDuree() . 'h</p>';
+        // affiche les détails d’une activité et propose le formulaire de réservation.
+    }
+} else {
+    echo '<p>Activité introuvable</p>';
 }
-// else {
-//     echo '<p>Activité introuvable</p>';
-// }
