0baafc2452bad682523a98d813d8102fc25cce3a
Leogeox <leovginx@gmail.com>
2025-11-28 15:41:00 +0100

    modified activity/one.php

--- app/views/activity/one.php
+++ app/views/activity/one.php
@@ -4,6 +4,7 @@ if (count($activities) > 0) {
         echo '<h3>' . $activity->getNom() . '</h3>';
         echo '<p>' . $activity->getDescription() . '</p>';
         echo '<p> Places disponible : ' . $activity->getPlacesDisponibles() . '</p>';
+        // Doit changer place dispo => ajouter places en data base et apres faire place prise
         echo '<p> Heure début : ' . $activity->getDateTimeDebut() . ' h</p>';
         echo '<p> Durée : ' . $activity->getDuree() . 'h</p>';
         // affiche les détails d’une activité et propose le formulaire de réservation.
